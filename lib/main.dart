7c61bdf73fd602b0d929434a3fa5ba3615f30b12
death3721 <410282238@qq.com>
2025-07-25 12:58:06 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:sizer/sizer.dart';
-
 import 'page/bottom_navigation_bar.dart';
 
 Future<void> main() async {
   await GetStorage.init();
+
   runApp(
-    Sizer(
-      builder: (context, orientation, deviceType) {
-        return GetMaterialApp(
-          home: BottomNavigationBarPage(),
-          theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.indigo,
-            ),
-          ),
-        );
-      },
+    GetMaterialApp(
+      home: BottomNavigationBarPage(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
     ),
   );
 }
