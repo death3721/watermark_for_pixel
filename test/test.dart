5d52d7f259d344b3a61e460cc3b45ae9984270cd
death3721 <410282238@qq.com>
2025-07-25 12:40:05 +0900

    update

---
+++ test/test.dart
@@ -0,0 +1,12 @@
+// test/counter_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('加法测试', () {
+    // final counter = Counter();
+
+    // counter.increment();
+
+    // expect(counter.value, 1);
+  });
+}
